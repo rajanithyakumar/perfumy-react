212f2aba83a4a7734ff6ab661f8c2576e5b73ffb
rajanithyakumar <rajanithyakumar18@gmail.com>
2026-04-24 18:46:43 +0530

    initial commit with full project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import Navbar from './components/Navbar'
+import Search from"./components/Search"
+import Product from "./components/Product"
+import Footer from "./components/Footer"
+import About from "./components/About"
+
+
+function App()
+{
+    return(
+        <div>
+            
+   <Navbar/>
+   <Search/>
+   <Product/>
+   <About/>
+   <Footer/>
+
+
+        </div>
+    )
+
+    
+}
+
+export default App
